05fa57d0e087e177292f001f87dd6b4c2f7944a0
Chandan Kumar <solouser019@gmail.com>
2026-08-05 10:30:00 +0530

    Add initial implementation of type casting example in C++

---
+++ ApnaCollege/variable&DataTypes/code.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+// int main(){
+//     cout << "Hello world \n Apna College " << endl;
+//     return 0;
+// }
+
+// int main(){
+//     int age = 24;
+//     cout << age << endl;
+//     return 0;
+// }
+
+// input 
+// int main(){
+//     int age;
+//     cout << "Enter your age ";
+//     cin >> age;
+
+//     cout << "Your age is : " << age << endl;
+//     return 0;
+// }  
+
+
+// sum of 2 numbers 
+// int main(){
+//     int a = 8, b = 4;
+//     cout << "Sum = " << (a + b) << endl ;
+//     cout << "Difference = " << (a - b) << endl;
+//     cout << "Multiply = " << (a * b) << endl;
+//     cout << "Divide = " << (a / b) << endl;
+//     return 0;
+// }
+
+// type casting 
+int main(){
+    int a = 52;
+    double b = 5;
+    cout << (a / b) << endl;
+    cout << (5 / (double)2) << endl;
+    return 0;
+}
