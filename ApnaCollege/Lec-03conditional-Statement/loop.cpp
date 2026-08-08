bee7a29944270bcddd1048bd7ccfbe96ba97c7a9
Chandan Kumar <solouser019@gmail.com>
2026-08-08 22:48:11 +0530

    Add initial implementation of loop example in C++

---
+++ ApnaCollege/Lec-03conditional-Statement/loop.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int count = 1;
+
+    while(count <= 8){
+        cout << count << " ";
+        count++;
+    }
+    cout << endl;
+    return 0;
+}
