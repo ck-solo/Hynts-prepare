fe28515a794f45d9af7ed686aea37959eb276ead
Chandan Kumar <solouser019@gmail.com>
2026-08-08 21:04:08 +0530

    Add examples for type casting and conditional statements in C++

---
+++ ApnaCollege/Lec-03conditional-Statement/code.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+// int main(){
+    
+//     int age ;
+//     cout << "Enter Your age: ";
+//     cin >> age;
+//     if(age >= 18){
+//         cout << "You are eligible for Vote";
+//     }else{
+//         cout << "Your are not eleigible for vote";
+//     }
+//     return 0;
+ 
+// }
+
+// *************************************************************
+
+int main(){    
+    int n ;
+    cout << "Enter a nummber: ";
+    cin >> n;
+    if(n%2 == 0){
+        cout << "Even number";
+    }else{
+        cout << "Odd number";
+    }
+    return 0;
+ 
+}
