2ec0f4fe2206608238e4538a1b74afb3240e63b4
Chandan Kumar <solouser019@gmail.com>
2026-08-14 21:34:31 +0530

    feat: update C++ program to print numbers in a grid format

--- ApnaCollege/Lec-04Patters/code.cpp
+++ ApnaCollege/Lec-04Patters/code.cpp
@@ -1,14 +1,42 @@
 #include <iostream>
 using namespace std;
 
+// int main(){
+//     int n = 4;
+//     for(int i =1; i<=n; i++){
+//         for(int j = 1; j <= n; j++){
+//             cout <<  "* ";
+//         }
+//         cout << endl;
+//     }
+
+//     return 0;
+// }
+
+// int main(){
+//     int n = 4;
+//     for(int i = 0; i<=n; i++){
+//         char ch = 'A';
+//         for(int j =0; j<=n; j++){
+//             cout << ch << " ";
+//             ch++;
+//         }
+//         cout << endl;
+
+//     }
+//     return 0;
+
+// }
+
 int main(){
-    int n = 4;
-    for(int i =1; i<=n; i++){
-        for(int j = 1; j <= n; j++){
-            cout <<  "* ";
+    int n = 3;
+    int count = 1;
+    for(int i = 0; i<n; i++){
+        for(int j = 0; j<n ; j++){
+            cout << count << " "; 
+            count++;   
         }
         cout << endl;
     }
-
     return 0;
 }
