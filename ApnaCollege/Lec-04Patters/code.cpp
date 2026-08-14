9d09025a6173fa3906cbbbd229536802df66e7a2
Chandan Kumar <solouser019@gmail.com>
2026-08-14 23:31:33 +0530

    feat: add new C++ programs for number patterns and reverse number display

--- ApnaCollege/Lec-04Patters/code.cpp
+++ ApnaCollege/Lec-04Patters/code.cpp
@@ -98,3 +98,59 @@ using namespace std;
 //     }
 //     return 0;
 // }
+
+
+
+// int main(){
+//     int n = 3;
+//     for(int i = 0; i<n; i++){
+//         for(int j = 1; j<= i+1; j++){
+//             cout << j << " ";
+//         }
+//     cout << endl;
+//     }
+
+//     return 0;
+// }
+
+
+
+// int main(){
+//     int n = 3;
+//     for(int i = 0; i<n; i++){
+//         for(int j = 1; j<= i+1; j++){
+//             cout << j << " ";
+//         }
+//     cout << endl;
+//     }
+
+//     return 0;
+// }
+
+
+// Reverse number
+// int main(){
+//     int n = 3;
+//     for(int i = 0; i<n; i++){
+//         for(int j = i+1; j>0; j--){
+//             cout << j << " ";
+//         }
+//         cout << endl;
+//     }
+//     return 0;
+// }
+
+
+
+// int main(){
+//     int n = 3;
+//     int num = 1;
+//     for(int i =0; i<=n; i++){
+//         for(int j =1; j<= i+ 1; j++){
+//             cout << num << " ";
+//             num++;
+//         }
+//     cout << endl;
+//     }
+//     return 0;
+// }
