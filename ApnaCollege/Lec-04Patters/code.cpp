bd604fa068d27f71a80e4498d0014994df690885
Chandan Kumar <solouser019@gmail.com>
2026-08-14 22:10:15 +0530

    feat: add additional C++ programs for various patterns and number displays

--- ApnaCollege/Lec-04Patters/code.cpp
+++ ApnaCollege/Lec-04Patters/code.cpp
@@ -28,15 +28,59 @@ using namespace std;
 
 // }
 
-int main(){
-    int n = 3;
-    int count = 1;
-    for(int i = 0; i<n; i++){
-        for(int j = 0; j<n ; j++){
-            cout << count << " "; 
-            count++;   
-        }
-        cout << endl;
-    }
-    return 0;
-}
+
+
+
+// int main(){
+//     int n = 3;
+//     int count = 1;
+//     for(int i = 0; i<n; i++){
+//         for(int j = 0; j<n ; j++){
+//             cout << count << " "; 
+//             count++;   
+//         }
+//         cout << endl;
+//     }
+//     return 0;
+// }
+
+
+// int main(){
+//     int n = 3;
+//     char ch = 'A';
+//     for(int i = 0; i<=n; i++){
+//         for(int j = 0; j<n; j++){
+//             cout << ch << " ";
+//             ch++;
+//         }
+//         cout << endl;
+//     }
+//     return 0;
+// }
+
+
+
+
+// int main(){
+//     int n = 4;
+//      for(int i = 0; i<n; i++){
+//         for(int j = 0; j < i+ 1 ; j++){
+//             cout << "* " << " ";
+//         }
+//         cout << endl;
+//      }
+//      return 0;
+// }
+
+
+
+// int main(){
+//     int n = 3;
+//     for(int i = 0; i<=n; i++){
+//         for(int j = 0; j<i+1; j++){
+//             cout<< i+1 << " ";
+//         }
+//         cout << endl;
+//     }
+//     return 0;
+// }
