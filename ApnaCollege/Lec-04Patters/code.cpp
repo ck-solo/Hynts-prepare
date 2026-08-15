bce10ac1c1d2b46993d913dff66d657375e34106
Chandan Kumar <solouser019@gmail.com>
2026-08-15 21:56:12 +0530

    feat: add inverted alphabet triangle pattern implementation and boilerplate files

--- ApnaCollege/Lec-04Patters/code.cpp
+++ ApnaCollege/Lec-04Patters/code.cpp
@@ -173,10 +173,31 @@ using namespace std;
 
 
 // Inverted Traingle Pattern 
+// int main(){
+//     int n = 4;
+//     for(int i = 0; i<=n; i++){
+//         for(int j = 0; j<= i; j++){
+//             cout << " ";
+//         } 
+//         for(int j = 0; j< n-i; j++){
+//             cout << (i + 1);
+//         }
+//         cout << endl;
+//     }
+//     return 0;
+// }
+
+// Inverted Alphabet Traingle Pattern
 int main(){
-    for(int i = 0; i<=n; i++){
-        for(int j = 0; j<= i; j++){
-            cout << "* " << " ";
+    int n = 4;
+    char ch = 'A';
+    for(int i =0; i<n; i++){
+        for(int j = 0; j< i; j++){
+            cout << " ";
+        }
+        for(int j = 0; j<n-i; j++){
+            cout << char(ch+ j);
+            
         }
         cout << endl;
     }
