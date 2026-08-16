b0c5a2fbec986a92a0c4144da3ce6a12163d9e08
Chandan Kumar <solouser019@gmail.com>
2026-08-16 23:50:57 +0530

    feat: implement nCr binomial coefficient calculation using factorial function

--- ApnaCollege/Lec-05Function/code.cpp
+++ ApnaCollege/Lec-05Function/code.cpp
@@ -43,3 +43,48 @@ using namespace std;
 //     cout << factorial(5) << endl;
 //     return 0;
 // }
+
+
+// Sum of digit of number 
+// int sumDigit(int n){
+//     int sum = 0;
+//     while(n > 0){
+//         int lastDigit = n % 10;
+//         n = n / 10;
+//         sum = sum + lastDigit;
+//     }
+//     return sum;
+// }
+
+// int main(){
+//     cout << sumDigit(145) << endl;
+
+//     return 0;
+// }
+
+
+
+// nCr Binomial coefficient for n & r
+
+int factorial(int n){
+    int fact = 1;
+    for(int i = 1; i <=n; i++){
+        fact = fact * i;
+    }
+    return fact;
+}
+
+int nCr(int n, int r ){
+     int fact_n = factorial(n);
+     int fact_r = factorial(r);
+     int fact_n_minus_r = factorial(n-r); 
+     return (fact_n/ (fact_r*fact_n_minus_r));
+}
+
+int main(){
+    int n = 5;
+    int r = 3;
+    
+    cout << nCr(n,r) << endl;
+    return 0;
+}
