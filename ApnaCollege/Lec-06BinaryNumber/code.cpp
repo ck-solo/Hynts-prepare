2f39037c57b8e3a9a80882887dd62f7602737d6f
Chandan Kumar <solouser019@gmail.com>
2026-08-21 17:58:45 +0530

    feat: add binary to decimal conversion implementation and compiled executable

--- ApnaCollege/Lec-06BinaryNumber/code.cpp
+++ ApnaCollege/Lec-06BinaryNumber/code.cpp
@@ -1,20 +1,37 @@
 #include <iostream>
 using namespace std;
 
-int decToBinary(int decNum){
-    int ans = 0, pow = 1;
+// int decToBinary(int decNum){
+//     int ans = 0, pow = 1;
     
-    while(decNum > 0){
-        int rem = decNum % 2;
-        decNum = decNum/2;
-        ans = ans + rem*pow;
-        pow = pow * 10; 
+//     while(decNum > 0){
+//         int rem = decNum % 2;
+//         decNum = decNum/2;
+//         ans = ans + rem*pow;
+//         pow = pow * 10; 
 
+//     }
+//     return ans;
+// }
+
+// int main(){
+//     cout << decToBinary(10) << endl;
+//     return 0;
+// }
+
+
+int bintoDeimal(int binNum){
+    int ans = 0; int pow = 1;
+    while(binNum>0){
+        int rem = binNum%10;
+        binNum = binNum/10;
+        ans = ans + rem * pow;
+        pow = pow * 2;
     }
     return ans;
 }
 
 int main(){
-    cout << decToBinary(10) << endl;
+    cout << bintoDeimal(1010) << endl;
     return 0;
 }
