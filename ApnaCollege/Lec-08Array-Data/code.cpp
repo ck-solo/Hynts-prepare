b8d2d9ae7b46baa5424bff9ae887389dbde7d932
Chandan Kumar <solouser019@gmail.com>
2026-08-22 19:11:12 +0530

    feat: add C++ program to demonstrate array manipulation by doubling mark values

---
+++ ApnaCollege/Lec-08Array-Data/code.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int marks[5] = {97, 85, 78, 94, 99};
+    int double_marks[5];
+
+    for (int i = 0; i < 5; i++){
+        double_marks[i] = marks[i] * 2;
+    }
+
+    for (int i = 0; i < 5; i++){
+        cout << double_marks[i] << " ";
+    }
+
+    return 0;
+}
