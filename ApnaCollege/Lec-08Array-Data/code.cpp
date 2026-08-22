000954737e081c7139fa797057220dade749abc1
Chandan Kumar <solouser019@gmail.com>
2026-08-22 19:28:39 +0530

    feat: add array traversal example for lecture 08

--- ApnaCollege/Lec-08Array-Data/code.cpp
+++ ApnaCollege/Lec-08Array-Data/code.cpp
@@ -1,17 +1,28 @@
 #include <iostream>
 using namespace std;
 
+// int main(){
+//     int marks[5] = {97, 85, 78, 94, 99};
+//     int double_marks[5];
+
+//     for (int i = 0; i < 5; i++){
+//         double_marks[i] = marks[i] * 2;
+//     }
+
+//     for (int i = 0; i < 5; i++){
+//         cout << double_marks[i] << " ";
+//     }
+
+//     return 0;
+// }
+
+
 int main(){
-    int marks[5] = {97, 85, 78, 94, 99};
-    int double_marks[5];
-
-    for (int i = 0; i < 5; i++){
-        double_marks[i] = marks[i] * 2;
+    int marks[] = {99, 100, 54, 36, 88};
+    int size = 5; 
+    // cout << sizeof(marks) << endl;
+    for(int i = 0; i<size; i++){
+        cout << marks[i] << endl;
     }
-
-    for (int i = 0; i < 5; i++){
-        cout << double_marks[i] << " ";
-    }
-
     return 0;
-}
+} 
