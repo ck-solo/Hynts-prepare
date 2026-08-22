cae338203eefd7208c7c2984d013159abd1f63e5
Chandan Kumar <solouser019@gmail.com>
2026-08-22 18:30:57 +0530

    feat: implement function to check if a number is a power of two using bitwise operators

--- ApnaCollege/Lec-07Bitwise/code.cpp
+++ ApnaCollege/Lec-07Bitwise/code.cpp
@@ -1,13 +1,32 @@
 #include <iostream>
 using namespace std;
 
-int main(){
+// int main(){
 
-    int a = 4, b = 8;
-    cout << (a&b) << endl; 
-    cout << (a|b) << endl; 
-    cout << (a^b) << endl; 
+//     int a = 4, b = 8;
+//     cout << (a&b) << endl; 
+//     cout << (a|b) << endl; 
+//     cout << (a^b) << endl; 
   
 
+//     return 0;
+// }
+
+// Find out how to find if a number is power of 2 without any loop
+
+
+bool isPowerTwo(int n){
+    return n > 0  && (n & (n - 1)) == 0;
+
+}
+
+int main(){
+    int n = 18;
+    
+    if(isPowerTwo(n))
+        cout << "Power of 2";
+        else 
+            cout << "Not a power of 2.";
+        
     return 0;
 }
