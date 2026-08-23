61b69faf708a51e2eaabac8c16680e50ea78fd02
Chandan Kumar <solouser019@gmail.com>
2026-08-23 20:07:37 +0530

    feat: add implementation to find smallest and largest elements in an array

--- ApnaCollege/Lec-08Array-Data/code.cpp
+++ ApnaCollege/Lec-08Array-Data/code.cpp
@@ -36,11 +36,12 @@ int main(){
     int nums[] = {5, 15, 22, 1, -15,24};
     int size = 6;
     int smallest = INT_MAX;
+    int largest = INT_MIN;
     for(int i =0; i<= size; i++){
-        if(nums[i] < smallest){
-            smallest = nums[i];
-        }
+        smallest = min(nums[i], smallest);
+        largest = max(nums[i], largest);
     }    
     cout << "Smallest : " << smallest << endl;
+    cout << "Largest : " << largest << endl;
     return 0;
 }
