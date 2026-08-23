a21222b7b6e4852482874a29a35925b3a230dcf1
Chandan Kumar <solouser019@gmail.com>
2026-08-23 20:04:26 +0530

    feat: add C++ program to find the smallest element in an array

--- ApnaCollege/Lec-08Array-Data/code.cpp
+++ ApnaCollege/Lec-08Array-Data/code.cpp
@@ -17,14 +17,30 @@ using namespace std;
 // }
 
 
+// int main(){
+//     int size = 5; 
+//     int marks[size] ; 
+//     for(int i = 0; i<size; i++){
+//         cin >> marks[i];
+//     }
+//     for(int i = 0; i<size; i++){
+//         cout << marks[i] << endl;
+//     }
+//     return 0;
+// } 
+
+
+// Find smallest / largest in Array
+
 int main(){
-    int size = 5; 
-    int marks[size] ; 
-    for(int i = 0; i<size; i++){
-        cin >> marks[i];
-    }
-    for(int i = 0; i<size; i++){
-        cout << marks[i] << endl;
-    }
+    int nums[] = {5, 15, 22, 1, -15,24};
+    int size = 6;
+    int smallest = INT_MAX;
+    for(int i =0; i<= size; i++){
+        if(nums[i] < smallest){
+            smallest = nums[i];
+        }
+    }    
+    cout << "Smallest : " << smallest << endl;
     return 0;
-} 
+}
