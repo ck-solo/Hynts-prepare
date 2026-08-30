2b6cd7b139b67d13bcc9194a2a02024f8327afb0
Chandan Kumar <solouser019@gmail.com>
2026-08-30 19:09:47 +0530

    feat: implement function to find first repeated element in an array

--- ApnaCollege/Lec-08Array-Data/code.cpp
+++ ApnaCollege/Lec-08Array-Data/code.cpp
@@ -337,16 +337,34 @@ using namespace std;
 
 
 // Rotate array 
+// int main() {
+//     int nums[] = {1, 2, 3, 4, 5};
+//     int n = sizeof(nums) / sizeof(nums[0]);
+//     int first = nums[0];
+//     for (int i = 0; i < n - 1; i++) {
+//         nums[i] = nums[i + 1];
+//     }
+//     nums[n - 1] = first;
+//     for (int i = 0; i < n; i++) {
+//         cout << nums[i] << " ";
+//     }
+//     return 0;
+// }
+
+
+// repeated first element
 int main() {
-    int nums[] = {1, 2, 3, 4, 5};
+    int nums[] = {10, 5, 3, 4, 3, 5};
     int n = sizeof(nums) / sizeof(nums[0]);
-    int first = nums[0];
-    for (int i = 0; i < n - 1; i++) {
-        nums[i] = nums[i + 1];
-    }
-    nums[n - 1] = first;
     for (int i = 0; i < n; i++) {
-        cout << nums[i] << " ";
+        for (int j = i + 1; j < n; j++) {
+            if (nums[i] == nums[j]) {
+                cout << "First repeated element: "
+                     << nums[i];
+                return 0;
+            }
+        }
     }
+    cout << "No repeated element";
     return 0;
 }
