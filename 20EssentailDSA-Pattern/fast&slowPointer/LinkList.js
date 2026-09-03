a8e62d31682e72a64761ac738fcdbafb1575c526
Chandan Kumar <solouser019@gmail.com>
2026-09-03 19:37:57 +0530

    feat: add ListNode utility functions for linked list traversal and node counting

--- 20EssentailDSA-Pattern/fast&slowPointer/LinkList.js
+++ 20EssentailDSA-Pattern/fast&slowPointer/LinkList.js
@@ -19,27 +19,40 @@
 
 // print all elements of a linked list
 
-function ListNode(val) {
-    this.val = val;
-    this.next = null;
-}
-var printlist = function(head) {
+// function ListNode(val) {
+//     this.val = val;
+//     this.next = null;
+// }
+// var printlist = function(head) {
+//     let current = head;
+//     while (current !== null) {
+//         console.log(current.val);
+//         current = current.next;
+//     }
+// };
+// let node1 = new ListNode(10);
+// let node2 = new ListNode(20);
+// let node3 = new ListNode(30);
+// let node4 = new ListNode(40);
+// node1.next = node2;
+// node2.next = node3;
+// node3.next = node4;
+// let head = node1;
+// printlist(head);
+// 10 → 20 → 30 → 40 → null
+
+
+
+// count the nodes 
+function countNodes(head) {
     let current = head;
+    let count = 0;
     while (current !== null) {
-        console.log(current.val);
+        count++;
         current = current.next;
     }
-};
+    return count;
+}
 
-let node1 = new ListNode(10);
-let node2 = new ListNode(20);
-let node3 = new ListNode(30);
-let node4 = new ListNode(40);
-node1.next = node2;
-node2.next = node3;
-node3.next = node4;
 
-let head = node1;
 
-printlist(head);
-// 10 → 20 → 30 → 40 → null
