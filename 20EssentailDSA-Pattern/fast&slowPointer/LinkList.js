1863c1e87601b8ab57449d3501356d84789d5f35
Chandan Kumar <solouser019@gmail.com>
2026-09-05 19:22:56 +0530

    revise the link list

--- 20EssentailDSA-Pattern/fast&slowPointer/LinkList.js
+++ 20EssentailDSA-Pattern/fast&slowPointer/LinkList.js
@@ -97,3 +97,24 @@ function findMax(head) {
 }
 
 
+// revise 
+
+// var LinkCycle = function(head){
+//     let slow = head;
+//     let fast = head;
+//     while(fast !== null && fast.next !== null){
+//         slow = slow.next;
+//         fast = fast.next.next;
+//         if(slow === fast){
+//             let start = head;
+//             while(start !== slow){
+//                 start = start.next;
+//                 slow = slow.next;
+//             }
+//             return start;
+//         }
+//     }
+//     return nulll;
+// }
+
+
